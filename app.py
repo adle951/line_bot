13e0c64844084c59f5bd9bc170756ba98a221d32
jing chen <adle9511@gmail.com>
2022-08-20 22:42:44 +0800

    fix requirements 2

--- app.py
+++ app.py
@@ -38,9 +38,17 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
+    r = '我看不懂你在說甚麼'
+    if msg == "你吃飯了嗎?":
+        r == "還沒"
+    if '如何' in msg:
+        r == '還不錯'
+
+
+
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text= '你吃飯了嗎?'))
+        TextSendMessage(text=r))
 
 
 if __name__ == "__main__":
